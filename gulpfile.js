6a4812b52d08283d9ba65c87cbab388a04d21bf6
Ivan <63046443+Ivan999@users.noreply.github.com>
2022-02-02 19:17:52 +0300

    Правки в проекте

--- gulpfile.js
+++ gulpfile.js
@@ -121,7 +121,7 @@ const server = (done) => {
 
 // Reload
 
-const reload = () => {
+const reload = (done) => {
   browser.reload();
   done();
 }
@@ -134,7 +134,7 @@ const watcher = () => {
   gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
-//Build
+// Build
 
 export const build = gulp.series(
   clean,
